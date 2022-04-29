4370429868c5ce9092c4df4141cbdb39afd43a88
leeuihyun <meta212121@naver.com>
2022-04-29 20:07:28 +0900

    edit uploadProduct page

---
+++ carrot/pages/uploadProduct.js
@@ -0,0 +1,8 @@
+import React from "react";
+import UploadProduct from "../components/UploadProduct";
+
+const uploadProducts = () => {
+    return <UploadProduct></UploadProduct>;
+};
+
+export default uploadProducts;
