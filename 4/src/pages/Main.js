6f168220d4ad0c40c23cbf8ec0ff9f0eff35f1cb
leeuihyun <meta212121@naver.com>
2022-05-16 23:58:14 +0900

    메인페이지 수정 및 계획중

--- 4/src/pages/Main.js
+++ 4/src/pages/Main.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Main() {
-    return <div>Main</div>;
+    return (
+        <div>
+            <div>메인헤더</div>
+            <div>전체화면 이미지 및 홍보</div>
+            <div>개발자소개</div>
+            <div>어떤식으로 개발했는가?</div>
+        </div>
+    );
 }
 
 export default Main;
