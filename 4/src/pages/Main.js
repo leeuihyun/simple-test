bcc0daaf320809ab3f9735837ac7492e4516e529
leeuihyun <meta212121@naver.com>
2022-05-18 23:48:36 +0900

    🔥 Main Page

--- 4/src/pages/Main.js
+++ 4/src/pages/Main.js
@@ -1,15 +1,13 @@
 import React, { useCallback } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { LOG_IN_REQUEST } from "../reducers/user";
+import styled from "styled-components";
+import MainLayout from "../component/MainLayout";
 
 function Main() {
-    const dispatch = useDispatch();
     return (
         <div>
-            <div>메인헤더</div>
-            <div>전체화면 이미지 및 홍보</div>
-            <div>개발자소개</div>
-            <div>어떤식으로 개발했는가?</div>
+            <MainLayout></MainLayout>
         </div>
     );
 }
