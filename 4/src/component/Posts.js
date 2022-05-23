6c19f90a355282744bca29ab5823b5e361b6eb8b
leeuihyun <meta212121@naver.com>
2022-05-23 22:14:47 +0900

    edit Posts component

--- 4/src/component/Posts.js
+++ 4/src/component/Posts.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function Posts({ data }) {
     return (
         <div>
             {data.map((v) => (
-                <div key={v.id}>{v.title}</div>
+                <div key={uuidv4()}>
+                    <img
+                        src={v.Data.file.preview}
+                        alt="preview"
+                        width="100"
+                        height="100"
+                    />
+                </div>
             ))}
         </div>
     );
