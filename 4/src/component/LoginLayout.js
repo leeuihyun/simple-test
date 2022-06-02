65b1b36922c457f4e2ad436ad6b7a49dc146bc36
leeuihyun <meta212121@naver.com>
2022-06-02 19:59:30 +0900

    fix styled-component

--- 4/src/component/LoginLayout.js
+++ 4/src/component/LoginLayout.js
@@ -9,10 +9,30 @@ import { Link } from "react-router-dom";
 const Container = styled.div`
     width: 100%;
     height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+const FormHeaderFirst = styled.div`
+    display: flex;
+    margin-top: 10rem;
+    width: 50vw;
+    height: 10vh;
+    justify-content: center;
+    align-items: center;
+    background-color: blue;
+    color: red;
 `;
 const Form = styled.div`
-    margin-top: 5rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 50vw;
+    height: 50vh;
+    background-color: black;
+    color: white;
 `;
+
 function LoginLayout() {
     const [email, onChangeEmail, setEmail] = useTool("");
     const [password, onChangePassword, setPassword] = useTool("");
@@ -43,33 +63,37 @@ function LoginLayout() {
                     </ul>
                 </div>
             </Header>
-            <Form>
-                <form onSubmit={onSubmitForm}>
-                    <div>
-                        <label htmlFor="email">이메일</label>
-                        <input
-                            type="text"
-                            name="email"
-                            value={email}
-                            id="email"
-                            onChange={onChangeEmail}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="password">비밀번호</label>
-                        <input
-                            type="text"
-                            name="password"
-                            value={password}
-                            id="password"
-                            onChange={onChangePassword}
-                        />
-                    </div>
-                    <div>
-                        <button type="submit">로그인</button>
-                    </div>
-                </form>
-            </Form>
+
+            <div>
+                <FormHeaderFirst>asdkfjdsalfjdas</FormHeaderFirst>
+                <Form>
+                    <form onSubmit={onSubmitForm}>
+                        <div>
+                            <label htmlFor="email">이메일</label>
+                            <input
+                                type="text"
+                                name="email"
+                                value={email}
+                                id="email"
+                                onChange={onChangeEmail}
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="password">비밀번호</label>
+                            <input
+                                type="text"
+                                name="password"
+                                value={password}
+                                id="password"
+                                onChange={onChangePassword}
+                            />
+                        </div>
+                        <div>
+                            <button type="submit">로그인</button>
+                        </div>
+                    </form>
+                </Form>
+            </div>
         </Container>
     );
 }
