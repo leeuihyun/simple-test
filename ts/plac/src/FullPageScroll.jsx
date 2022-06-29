addd8009444d657df14a2211372ab209fb8a0be6
leeuihyun <meta212121@naver.com>
2022-06-29 20:14:57 +0900

    plac 기본 틀

--- ts/plac/src/FullPageScroll.jsx
+++ ts/plac/src/FullPageScroll.jsx
@@ -4,22 +4,33 @@ import FullPage, {
     FullpageSection,
     FullPageNavigation,
 } from "@ap.cx/react-fullpage";
+import Header from "./components/Header";
 
 const FullPageScroll = () => {
+    const sectionStyle = {
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+    };
     return (
-        <FullPage>
-            <FullPageSections>
-                <FullpageSection style={{ height: "100vh" }}>
-                    <h1>screen 1</h1>
-                </FullpageSection>
-                <FullpageSection style={{ height: "100vh" }}>
-                    <h1>screen 2</h1>
-                </FullpageSection>
-                <FullpageSection style={{ height: "100vh" }}>
-                    <h1>screen 3</h1>
-                </FullpageSection>
-            </FullPageSections>
-        </FullPage>
+        <div>
+            <Header />
+            <FullPage>
+                <FullPageSections>
+                    <FullpageSection style={sectionStyle}>
+                        <h1>screen 1</h1>
+                    </FullpageSection>
+                    <FullpageSection style={sectionStyle}>
+                        <h1>screen 2</h1>
+                    </FullpageSection>
+                    <FullpageSection style={sectionStyle}>
+                        <h1>screen 3</h1>
+                    </FullpageSection>
+                </FullPageSections>
+            </FullPage>
+        </div>
     );
 };
 
---
+++ ts/plac/src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+const HeaderStyle = styled.div`
+    width: 100%;
+    //display: flex;
+    position: fixed;
+    text-align: center;
+    //letter-spacing: 0.2rem;
+`;
+
+const Menubar = styled.div``;
+
+const UserBar = styled.div``;
+const Header = () => {
+    return <HeaderStyle>PLAC</HeaderStyle>;
+};
+
+export default Header;
